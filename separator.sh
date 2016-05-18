51b751c01ab16ef57e8fc65ea5114892ee13866e
Danil Mikhailov <danil@altlinux.org>
2016-05-19 01:45:01 +0300

    Remove comments

--- separator.sh
+++ separator.sh
@@ -3,16 +3,12 @@
 outdir="./out"
 
 separate(){
-    #mkdir -p "$outdir"
-    #current_id="$(grep -Eo -m1 'event_key=[0-9]+' | sed "s|event_key=||g")"
-    #grep "$current_id" >> "$outdir/${current_id}.log"
     echo "$@" >> "$outdir/$(echo "$@" | grep -Eo -m1 'event_key=[0-9]+' | sed "s|event_key=||g").log"
 }
 
 read_all(){
     while read line
     do
-	#echo ${line} | separate
 	separate "${line}"
     done < "${1:-/dev/stdin}"
 }
@@ -24,6 +20,3 @@ my_test(){
 }
 
 my_test
-
-#cat example.log | ./separator.sh #run
-#separate <&0
