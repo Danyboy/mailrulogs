244432163f3bdd5051348d34a09180d678d18e62
Danil Mikhailov <danil@altlinux.org>
2016-05-23 21:54:19 +0300

    Ip: enable and test xargs

--- ip_separator.sh
+++ ip_separator.sh
@@ -7,19 +7,26 @@ separate_ip(){
     ipreg="([0-9]{1,3}[\.]){3}[0-9]{1,3}"
     regexp="^${mark}${ipreg}"
 
-    #xargs -n1 -P4 -I myline echo myline >> "$outdir/$(echo myline | grep -Eo -m1 "${regexp}").log"
-    #% sh -c 'command1; command2;'
-
+    #echo "$@" >> "$outdir/$(echo "$@" | grep -Eo -m1 "${regexp}").log"
+    
     cd $(dirname "$0")/${outdir}/
-    xargs -n1 -P4 -I file sh -c 'echo "file" >> $(echo "file"| cut -c1-15 | grep -Eo -m1 "^([0-9]{1,3}[\.]){3}[0-9]{1,3}").log'
-    #xargs -n1 -P4 -I file sh -c 'echo "file"; echo "file" | cut -c1-15 | grep -Eo -m1 "^([0-9]{1,3}[\.]){3}[0-9]{1,3}"'
+    xargs -n1 -P4 -I file sh -c 'echo "file" >> $(echo "file"| cut -c1-15 | grep -Eo -m1 "([0-9]{1,3}[\.]){3}[0-9]{1,3}").log'
+    #xargs -n1 -P4 -I file sh -c 'echo "file"; echo "file" | cut -c1-15 | grep -Eo -m1 "([0-9]{1,3}[\.]){3}[0-9]{1,3}"'
+    #xargs -n1 -P4 -I myline echo myline >> "$outdir/$(echo myline | grep -Eo -m1 "${regexp}").log"
     cd -
 }
+
+separate_ips(){
+    while read line ; do
+        separate_ip "${line}"
+    done < "${1:-/dev/stdin}"
+}
+
 
 my_test(){
     outdir=${outdir}/"$(date +%H%M)"
     mkdir -p "$outdir"
-    separate_ip
+    separate_ips
 }
 
 my_test
